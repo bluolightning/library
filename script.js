38c70cbb94b75bfa7dcb00f1239abc2718c60965
bluolightning <124224350+bluolightning@users.noreply.github.com>
2024-11-23 17:41:06 -0500

    Add read label and checkbox (no functionality)

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const library = [];
 function Book(title, author) {
     this.title = title;
     this.author = author;
+    this.readStatus = false;
 }
 
 function addBookToLibrary(title, author) {
@@ -28,19 +29,28 @@ document.querySelector(".refreshLibrary").addEventListener("click", function() {
     for (const book of library) {
         let h1 = document.createElement("h1");
         let h2 = document.createElement("h2");
-        let button = document.createElement("button")
+        let button = document.createElement("button");
+        let readLabel = document.createElement("label");
+        let checkbox = document.createElement("input");
+        let div = document.createElement("div");
         
         h1.textContent = book.title;
         h2.textContent = book.author;
         button.textContent = "Remove Book";
+        readLabel.textContent = "Read: ";
 
         button.setAttribute("order", "order" + orderNumber);
         button.setAttribute("class", "removeBook");
+        readLabel.setAttribute("for", "read");
+        checkbox.setAttribute("type", "checkbox");
 
 
         libraryDisplay.appendChild(h1);
         libraryDisplay.appendChild(h2);
         libraryDisplay.appendChild(button);
+        libraryDisplay.appendChild(div);
+        div.appendChild(readLabel);
+        div.appendChild(checkbox);
 
         // Remove books
         button.addEventListener("click", function() {
