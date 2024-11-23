8d08b8761a81677ca5f46748bf5f80c288ddc0ec
bluolightning <124224350+bluolightning@users.noreply.github.com>
2024-11-23 12:49:39 -0500

    Create a button that iterates through the library and logs each entry to the console

--- script.js
+++ script.js
@@ -8,10 +8,23 @@ function Book(title, author) {
 function addBookToLibrary(title, author) {
     const book = new Book(title, author);
     library.push(book);
-    console.log(book);
-    console.log(library);
 }
 
-document.querySelector("button").addEventListener("click", function() {
+document.querySelector(".addBook").addEventListener("click", function() {
     addBookToLibrary(prompt('title'), prompt('author'));
 });
+
+document.querySelector(".refreshLibrary").addEventListener("click", function() {
+    const libraryDisplay = document.querySelector(".libraryDisplay");
+
+    for (const book of library) {
+        console.log(book.title);
+        console.log(book.author);
+        console.log("");
+    }
+});
+
+addBookToLibrary("This is a book", "This is another one");
+addBookToLibrary("One Piece", "Oda");
+addBookToLibrary("Your Bud", "John James");
+
